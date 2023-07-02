12ce4f9792f5cea3f68e219e7ad70bbe8fbb694c
Michal Stachura <michal.stachura@santri.pl>
2023-07-02 18:42:27 +0200

    Start using process.env variables

--- src/stores/auth-store.ts
+++ src/stores/auth-store.ts
@@ -11,6 +11,10 @@ export const useAuthStore = defineStore('auth', () => {
   const router = useRouter();
   const $q = useQuasar();
 
+  if (process.env.APP_API_URL) {
+  }
+  console.log(process.env.APP_API_URL)
+
   const loggedUser = ref<User | null>(
     LocalStorage.getItem('sapp_user') === 'null'
       ? null
@@ -20,36 +24,35 @@ export const useAuthStore = defineStore('auth', () => {
   
   const openWebSocket = async () => {
     if (loggedUser.value) {
-      dashboardSocket.value = new WebSocket(
-        'wss://api.santri.it:62391/ws/dashboard/'
-      );
-
-      // dashboardSocket.value = new WebSocket(
-      //   'ws://127.0.0.1:8000/ws/dashboard/'
-      // );
-
-      dashboardSocket.value.onopen = () => {
-        dashboardSocket.value?.send(
-          JSON.stringify({ token: loggedUser.value?.token })
+      const wss = process.env.APP_WEBSOCKET_URL
+      if (wss) {
+        dashboardSocket.value = new WebSocket(
+          wss
         );
-      };
-
-      dashboardSocket.value.onmessage = (e) => {
-        const data = JSON.parse(e.data);
-        switch (data.type) {
-          case 'dashboard.notification':
-            console.log(data.message);
-            break;
-
-          default:
-            break;
-        }
-      };
-
-      dashboardSocket.value.onclose = (e) => {
-        console.log(e);
-        console.error('Dashboard socket closed unexpectedly');
-      };
+  
+        dashboardSocket.value.onopen = () => {
+          dashboardSocket.value?.send(
+            JSON.stringify({ token: loggedUser.value?.token })
+          );
+        };
+  
+        dashboardSocket.value.onmessage = (e) => {
+          const data = JSON.parse(e.data);
+          switch (data.type) {
+            case 'dashboard.notification':
+              console.log(data.message);
+              break;
+  
+            default:
+              break;
+          }
+        };
+  
+        dashboardSocket.value.onclose = (e) => {
+          console.log(e);
+          console.error('Dashboard socket closed unexpectedly');
+        };
+      }
     }
   };
 
@@ -62,42 +65,43 @@ export const useAuthStore = defineStore('auth', () => {
 
   const loginUser = async (payload: LoginForm) => {
     Loading.show();
-    const url = 'https://api.santri.it:62391/api/auth-token/';
-    // const url = 'http://127.0.0.1:8000/api/auth-token/';
-    axios
-      .post(url, {
-        username: payload.username,
-        password: payload.password
-      })
-      .then((response) => {
-        if (response.status === 200) {
-          loggedUser.value = {
-            token: response.data.token
-          };
-          LocalStorage.set('sapp_user', loggedUser.value);
-          openWebSocket();
-          $q.notify({
-            color: 'green-4',
-            textColor: 'white',
-            icon: 'done',
-            message: 'Successfully logged in'
-          });
-
-          router.push('/').catch((error: Error) => {
+    const url = process.env.APP_API_URL;
+    if (url) {
+      axios
+        .post(url, {
+          username: payload.username,
+          password: payload.password
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            loggedUser.value = {
+              token: response.data.token
+            };
+            LocalStorage.set('sapp_user', loggedUser.value);
+            openWebSocket();
+            $q.notify({
+              color: 'green-4',
+              textColor: 'white',
+              icon: 'done',
+              message: 'Successfully logged in'
+            });
+  
+            router.push('/').catch((error: Error) => {
+              showErrorMessage(error.message);
+            });
+          }
+          Loading.hide();
+        })
+        .catch((error: Error | DrfError) => {
+          if ('response' in error) {
+            const errorData = error.response.data;
+            const message = errorData.non_field_errors.join(', ');
+            showErrorMessage(message);
+          } else {
             showErrorMessage(error.message);
-          });
-        }
-        Loading.hide();
-      })
-      .catch((error: Error | DrfError) => {
-        if ('response' in error) {
-          const errorData = error.response.data;
-          const message = errorData.non_field_errors.join(', ');
-          showErrorMessage(message);
-        } else {
-          showErrorMessage(error.message);
-        }
-      });
+          }
+        });
+    }
   };
 
   const logoutUser = async () => {
